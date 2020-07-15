d7be2aba8c6faa7d794a25190c84e72399c9c890
zheng <1123020105@qq.com>
2020-07-15 20:20:27 +0800

    调用了数组章节中的数组类

---
+++ Queue/src/Array.java
@@ -0,0 +1,161 @@
+public class Array<E> {
+
+    private E[] data;
+    private int size; //size表示数组中实际有多少个元素
+
+    //构造函数，传入数组的容量capacity构造Array
+    public Array(int capacity) { //capacity表示数组中最多可以装多少个元素
+        this.data = (E[])new Object[capacity]; //注意这里的写法，必须这么写
+        this.size = 0;
+    }
+
+    //无参的构造函数，默认数组的容量capacity=10
+    public Array() {
+        this(10);
+    }
+
+    //获取数组中元素的个数
+    public int getSize() {
+        return size;
+    }
+
+    //获取数组的容量
+    public int getCapacity() {
+        return data.length;
+    }
+
+    //返回数组是否为空
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    //向所有元素后添加一个新的元素
+    public void addLast(E e) {
+        add(size, e);
+    }
+
+    //在所有元素前添加一个新的元素
+    public void addFirst(E e) {
+        add(0, e);
+    }
+
+    //在第index位置插入一个新元素e
+    public void add(int index, E e) {
+        if (index < 0 || index > size) {
+            throw new IllegalArgumentException("Add failed. Required index >= 0 and index <= size");
+        }
+
+        if (size == data.length) {
+            resize(2 * data.length);
+        }
+
+        for (int i = size - 1; i >= index; i--) {
+            data[i + 1] = data[i];
+        }
+
+        data[index] = e;
+        size++;
+    }
+
+    //获取index索引位置的元素
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Get failed. Index is illegal");
+        }
+        return data[index];
+    }
+
+    public E getLast() {
+        return get(size - 1);
+    }
+
+    public E getFirst() {
+        return get(0);
+    }
+
+    //修改index索引位置的元素为e
+    public void set(int index, E e) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Set failed. Index is illegal");
+        }
+        data[index] = e;
+    }
+
+    //查找数组中是否有元素e
+    public boolean contains(E e) {
+        for (int i = 0; i < size; i++) {
+            if(data[i].equals(e))
+                return true;
+        }
+        return false;
+    }
+
+    //查找数组中元素e所在的索引，如果不存在元素e，则返回-1
+    public int find(E e) {
+        for (int i = 0; i < size; i++) {
+            if(data[i].equals(e))
+                return i;
+        }
+        return -1;
+    }
+
+    //从数组中删除index位置的元素，返回删除的元素
+    public E remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IllegalArgumentException("Remove failed. Index is Illegal");
+        }
+        E ret = data[index];
+        for (int i = index + 1; i < size; i++) {
+            data[i - 1] = data[i];
+        }
+        size--;
+        //此时data[size]还存着一个引用，java内存回收是不会自动去回收的，需要手动回收，这句话不是必须的
+        //之后也会被其他元素覆盖，所以到时候垃圾回收机制也会回收它
+        data[size] = null;
+        if(size == data.length / 4 && data.length / 2 != 0) {
+            resize(data.length / 2);
+        }
+        return ret;
+    }
+
+    //从数组中删除第一个元素，返回删除的元素
+    public E removeFirst() {
+        return remove(0);
+    }
+
+    //从数组中删除最后一个元素，返回删除的元素
+    public E removeLast() {
+        return remove(size - 1);
+    }
+
+    //从数组中删除元素e
+    public void removeElement(E e) {
+        int index = find(e);
+        if (index != -1) {
+            remove(index);
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Array: size = %d, capacity = %d\n", size, data.length));
+        res.append("[");
+        for (int i = 0; i < size; i++) {
+            res.append(data[i]);
+            if(i != size - 1) {
+                res.append(", ");
+            }
+        }
+        res.append("]");
+        return res.toString();
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[])new Object[newCapacity];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[i];
+        }
+        data = newData;
+    }
+}
