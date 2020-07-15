f4e8be3cbfbae1395e856a3c81a42c2df91dcf3d
zheng <1123020105@qq.com>
2020-07-15 20:26:57 +0800

    添加了循环队列

---
+++ Queue/src/LoopQueue.java
@@ -0,0 +1,90 @@
+public class LoopQueue<E> implements Queue<E>{
+
+    private E[] data;
+    //front指向队列第一个元素，tail指向最后一个元素的后一个位置（和size意义一样）
+    private int front, tail;
+    private int size; //队列中有多少个元素
+
+    public LoopQueue(int capacity) {
+        data = (E[])new Object[capacity + 1]; //因为要浪费一个空间
+        this.front = 0;
+        this.tail = 0;
+        this.size = 0;
+    }
+
+    public LoopQueue() {
+        this(10);
+    }
+
+    public int getCapacity() {
+        return data.length - 1;
+    }
+
+    @Override
+    public int getSize() {
+        return data.length - 1;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    @Override
+    public void enqueue(E e) {
+        if ((tail + 1) % data.length == front) {
+            resize(getCapacity() * 2);
+        }
+        data[tail] = e;
+        tail = (tail + 1) % data.length;
+        size++;
+    }
+
+    @Override
+    public E dequeue() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Cannot dequeue from an empty queue.");
+        }
+        E ret = data[front];
+        data[front] = null;
+        front = (front + 1) % data.length;
+        size--;
+        if(size == getCapacity() / 4 && getCapacity() / 2 != 0) {
+            resize(getCapacity() / 2);
+        }
+        return ret;
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("Queue is empty.");
+        }
+        return data[front];
+    }
+
+    private void resize(int newCapacity) {
+        E[] newData = (E[])new Object[newCapacity + 1];
+        for (int i = 0; i < size; i++) {
+            newData[i] = data[(i + front) % data.length];
+        }
+        data = newData;
+        front = 0;
+        tail = size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        res.append(String.format("Queue: size = %d, capacity = %d\n", size, getCapacity()));
+        res.append("front [");
+        for (int i = front; i != tail; i = (i + 1) % data.length) {
+            res.append(data[i]);
+            if((i + 1) % data.length != tail) {
+                res.append(", ");
+            }
+        }
+        res.append("] tali");
+        return res.toString();
+    }
+}
