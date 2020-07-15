039000c42e743c035997916ef59987ed85e10577
zheng <1123020105@qq.com>
2020-07-15 20:09:24 +0800

    leetcode第20题有效的括号AC代码

---
+++ Stack/src/Solution.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '[' || c == '{' || c == '(') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                if (c == ']' && stack.pop() != '[') {
+                    return false;
+                }
+                if (c == '}' && stack.pop() != '{') {
+                    return false;
+                }
+                if (c == ')' && stack.pop() != '(') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
