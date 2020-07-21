289f3d15f89e744c5f073b969a5807efdfc6942a
zheng <1123020105@qq.com>
2020-07-21 16:42:45 +0800

    第二版并查集

---
+++ UnionFind/src/UnionFind2.java
@@ -0,0 +1,49 @@
+//我们的第二版Union-Find
+public class UnionFind2 implements UF {
+    private int[] parent;
+
+    public UnionFind2(int size) {
+        parent = new int[size];
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return parent.length;
+    }
+
+    //查找过程，查找元素p所对应的集合编号
+    //O(h)复杂度，h为树的高度
+    private int find(int p) {
+        if (p < 0 || p >= parent.length) {
+            throw new IllegalArgumentException("p is out of bound.");
+        }
+
+        while (p != parent[p]) {
+            p = parent[p];
+        }
+
+        return p;
+    }
+
+    //查看元素p和元素q是否属于一个集合
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    @Override
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot) {
+            return;
+        }
+
+        parent[pRoot] = qRoot;
+    }
+
+}
