62d8aee301e6ae66cea32cbaf1dd43663090af4d
zheng <1123020105@qq.com>
2020-07-21 16:44:11 +0800

    第五版并查集

---
+++ UnionFind/src/UnionFind5.java
@@ -0,0 +1,61 @@
+//我们的第五版Union-Find
+public class UnionFind5 implements UF {
+    private int[] parent; //parent[i]表示第i个元素所指向的父节点
+    private int[] rank; //rank[i]表示以i为根的集合所表示的树的层数
+
+    public UnionFind5 (int size) {
+        parent = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < parent.length; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
+    }
+
+    @Override
+    public int getSize() {
+        return parent.length;
+    }
+
+    //查找过程，查找元素p所对应的集合编号
+    //O(h)复杂度，h为树的高度
+    private int find(int p) {
+        if (p < 0 || p >= parent.length) {
+            throw new IllegalArgumentException("p is out of bound.");
+        }
+
+        while (p != parent[p]) {
+            parent[p] = parent[parent[p]];
+            p = parent[p];
+        }
+
+        return p;
+    }
+
+    //查看元素p和元素q是否属于一个集合
+    @Override
+    public boolean isConnected(int p, int q) {
+        return find(p) == find(q);
+    }
+
+    @Override
+    public void unionElements(int p, int q) {
+        int pRoot = find(p);
+        int qRoot = find(q);
+
+        if (pRoot == qRoot) {
+            return;
+        }
+
+        //根据两个元素所在树的rank不同判断合并方向
+        //将rank低的集合合并到rank高的集合上
+        if (rank[pRoot] < rank[qRoot]) {
+            parent[pRoot] = qRoot;
+        } else if (rank[pRoot] > rank[qRoot]){
+            parent[qRoot] = pRoot;
+        } else {
+            parent[qRoot] = pRoot;
+            rank[pRoot] += 1;
+        }
+    }
+}
